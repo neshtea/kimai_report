c5ca7aeea7a33ebfb81cc3232881fffc92403847
Marco Schneider <marco.schneider@active-group.de>
2024-02-01 12:17:51 +0100

    date: make including hh-mm-ss optional, default to true + leadin zeroes.

--- lib/date.ml
+++ lib/date.ml
@@ -21,11 +21,15 @@ let from_string_exn date_string =
   | _ -> raise @@ Date_format_error date_string
 ;;
 
-let to_html5_string { year; month; day } =
+let to_html5_string ?(with_timezone = true) { year; month; day } =
   (* SEE
      https://github.com/kimai/kimai/blob/main/src/API/TimesheetController.php#L89
      for the weird formatting *)
-  Printf.sprintf "%i-%i-%iT00:00:00" year month day
+  Printf.sprintf
+    (if with_timezone then "%04d-%02d-%02dT23:59:59" else "%04d-%02d-%02d")
+    year
+    month
+    day
 ;;
 
 let of_ptime pt =
