02c6d4fd05951a4712ac4e9e6e9b4230e95aa910
Marcus Crestani <marcus.crestani@active-group.de>
2024-04-30 11:39:17 +0200

    Fall back to string splitting for showing start time in local time zone.

--- lib/entry.ml
+++ lib/entry.ml
@@ -1,5 +1,6 @@
 type t =
   { date : Ptime.t
+  ; start_string : string
   ; end_string : string
   ; duration : float
   ; activity : int option
@@ -31,8 +32,8 @@ let date_string { date; _ } =
   Ptime.to_rfc3339 date |> date_string_from_timestamp
 ;;
 
-let start_time_string { date; _ } =
-  Ptime.to_rfc3339 date |> time_string_from_timestamp
+let start_time_string { start_string; _ } =
+  time_string_from_timestamp start_string
 ;;
 
 let end_time_string { end_string; _ } = time_string_from_timestamp end_string
@@ -42,13 +43,15 @@ module D = Decoder.Yojson.Safe
 let decoder =
   let open D.Syntax in
   let* date = D.field "begin" Api.timestamp_decoder in
+  let* start_string = D.field "begin" D.string in
   let* end_string = D.field "end" D.string in
   let* description = D.optional @@ D.field "description" D.string in
   let* activity = D.optional @@ D.field "activity" D.int in
   let* duration' = D.field "duration" D.int in
   let duration = float_of_int duration' /. 60. /. 60. in
   let* project = D.optional @@ D.field "project" D.int in
-  D.return { date; end_string; duration; activity; description; project }
+  D.return
+    { date; start_string; end_string; duration; activity; description; project }
 ;;
 
 let encoder begin_date_time end_date_time project activity description =
